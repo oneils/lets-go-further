128c5ee129bc4d47be7d652dcb398b21ea120dda
oneils <lex.nox@gmail.com>
2022-07-08 10:32:28 +0200

    Chapter 3: added json enveloping

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -17,7 +17,9 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
-func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+type envelope map[string]any
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
 	js, err := json.Marshal(data)
 	if err != nil {
 		return err
