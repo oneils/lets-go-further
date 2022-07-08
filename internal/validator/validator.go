594130258dcc78d3b58a656a32e5dfff2d8aaf6c
oneils <lex.nox@gmail.com>
2022-07-08 15:59:30 +0200

    Chapter 4: added input json validation

---
+++ internal/validator/validator.go
@@ -0,0 +1,66 @@
+package validator
+
+import "regexp"
+
+// EmailRX is a regex for sanity checking the format of email addresses
+//is  taken from https://html.spec.whatwg.org/#valid-e-mail-address.
+var (
+	EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\. [a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+)
+
+// Validator type with validation errors.
+type Validator struct {
+	Errors map[string]string
+}
+
+// New create a new Validator instance.
+func New() *Validator {
+	return &Validator{
+		Errors: make(map[string]string),
+	}
+}
+
+// Valid return true if the Validator has no errors.
+func (v *Validator) Valid() bool {
+	return len(v.Errors) == 0
+}
+
+// AddError adds an error message to the Validator if such key does not exist.
+func (v *Validator) AddError(key, message string) {
+	if _, exists := v.Errors[key]; !exists {
+		v.Errors[key] = message
+	}
+}
+
+// Check adds an error message to the Validator if the condition is not 'ok'.
+func (v *Validator) Check(ok bool, key, message string) {
+	if !ok {
+		v.AddError(key, message)
+	}
+}
+
+// PermittedValue returns true if the value is in the list of permitted values.
+func PermittedValue[T comparable](value T, permittedValues ...T) bool {
+	for i := range permittedValues {
+		if value == permittedValues[i] {
+			return true
+		}
+	}
+	return false
+}
+
+// Matches returns true if the value matches the regex.
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
+}
+
+// Unique returns true if all values in a slice are unique
+func Unique[T comparable](values []T) bool {
+	uniqueValues := make(map[T]bool)
+
+	for _, value := range values {
+		uniqueValues[value] = true
+	}
+
+	return len(values) == len(uniqueValues)
+}
