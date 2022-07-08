ce04469ad22dfe89c33b5a82f0ac92562e554b34
oneils <lex.nox@gmail.com>
2022-07-08 15:18:04 +0200

    Chapter 4: added custom UnmarshalJSON for Runtime field

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -12,10 +12,10 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	//HTTP request body (note that the field names and types in the struct are a subset
 	// of the Movie struct fields).
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year"`
-		Runtime int32    `json:"runtime"`
-		Genres  []string `json:"genres"`
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
 	}
 	err := app.readJSON(w, r, &input)
 	if err != nil {
--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -3,7 +3,10 @@ package data
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntime = fmt.Errorf("invalid runtime format")
 
 // Runtime represents the runtime of a movie in minutes.
 type Runtime int32
@@ -14,4 +17,29 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	// must wrap the string in quotes to make it a valid JSON string
 	quotedJSONValue := strconv.Quote(json)
 	return []byte(quotedJSONValue), nil
+}
+
+// UnmarshalJSON unmarshals the JSON value into the Runtime type.
+// We need pointer here in order to modify the receiver
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	// remove the quotes from the JSON string
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntime
+	}
+
+	parts := strings.Split(unquotedJSONValue, " ")
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntime
+	}
+
+	//  parse the string containing the number into an int32
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntime
+	}
+
+	*r = Runtime(i)
+
+	return nil
 }
