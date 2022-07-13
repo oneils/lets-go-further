e3f53cdcf742e8040bb0bf33e7558baf46057c69
oneils <lex.nox@gmail.com>
2022-07-13 21:43:13 +0200

    Chapter 11: replaced global rate limiter by ip-based rate limiter

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"golang.org/x/time/rate"
+	"net"
 	"net/http"
+	"sync"
+	"time"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
@@ -20,15 +23,64 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 }
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
-	// Initialize a new rate limiter which allows an average of 2 requests per second,
-	//with a maximum of 4 requests in a single ‘burst’.
-	limiter := rate.NewLimiter(2, 4)
+
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+
+			// Lock the mutex to prevent any rate limiter checks from happening while
+			//the cleanup is taking place.
+			mu.Lock()
+
+			for ip, cl := range clients {
+				if time.Since(cl.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			mu.Unlock()
+		}
+	}()
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !limiter.Allow() {
+		// Get the client IP address from the request.
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
+
+		mu.Lock()
+
+		if _, found := clients[ip]; !found {
+			clients[ip] = &client{
+				limiter: rate.NewLimiter(2, 4),
+			}
+		}
+
+		// update the last seen time for the client.
+		clients[ip].lastSeen = time.Now()
+
+		if !clients[ip].limiter.Allow() {
+			mu.Unlock()
 			app.rateLimitExceededResponse(w, r)
 			return
 		}
+
+		// Very importantly, unlock the mutex before calling the next handler in the
+		// chain. Notice that we DON'T use defer to unlock the mutex, as that would mean
+		//that the mutex isn't unlocked until all the handlers downstream of this
+		// middleware have also returned.
+		mu.Unlock()
 		next.ServeHTTP(w, r)
 	})
 }
