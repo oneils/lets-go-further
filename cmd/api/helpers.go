756d33fefff52143bd987c6f877035ecfc412cb3
oneils <lex.nox@gmail.com>
2022-08-26 23:39:39 +0200

    Chapter 14: added helper function for running background functions with recovery

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -169,3 +169,18 @@ func (app *application) readInt(qs url.Values, key string, defaultValue int, v *
 
 	return i
 }
+
+func (app *application) background(fn func()) {
+	go func() {
+		// recover any panic
+		defer func() {
+			if err := recover(); err != nil {
+				app.logger.PrintError(fmt.Errorf("%s", err), nil)
+			}
+		}()
+
+		// run the function
+		fn()
+	}()
+
+}
